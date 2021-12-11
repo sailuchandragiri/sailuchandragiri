808ad08a55a988de387b5860cc39edb7d960797a
sailuc <sailuchandragiri2000@gmail.com>
2021-12-11 12:17:00 +0530

    files

---
+++ unit-5/Redux/retodo/src/components/TodoList.jsx
@@ -0,0 +1,32 @@
+import {useSelector} from "react-redux";
+import {useState,useEffect} from "react";
+import "./style.css"
+
+export const TodoList = () =>{
+    const [showtask,setShowtask] = useState(false);
+    const [todos,setTodos] = useState([]);
+    const todo = useSelector((state)=>state.todo)
+    useEffect(()=>{
+      setTodos(todo);
+      console.log(todos);
+    },[todo])
+    const handleToggle = id => {
+        const test = todo.map(item => item.id === id?{...item,status:!item.status}:item);
+        setTodos(test);
+      }
+
+    return <div id="layout">
+        {
+          todos.filter(item=>showtask?true:!item.status).map((item)=><>
+        <button onClick={()=>handleToggle(item.id)}>
+          <div style={{marginRight:"20px"}}>{item.title}
+          <span style={{paddingLeft:"330px",textAlign:"right",float:"right"}}>
+            {item.status? "Done":'Not Done'}</span></div>
+        </button>
+        </>)
+      }
+      <button style={{width: '80%',margin_top:'40px',margin:'auto',height:"50px",fontSize:"30px",backgroundColor:"pink"}} 
+      onClick={()=>setShowtask(!showtask)}>{showtask?"SHOW NOT COMPELETED TASKS":"SHOW ALL TASK"}</button>
+
+    </div>
+}
