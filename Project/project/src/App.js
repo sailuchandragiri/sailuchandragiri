a157ce2be1455d78dee946308cc2a979307e1405
sailuc <sailuchandragiri2000@gmail.com>
2021-12-25 13:12:21 +0530

    files

--- Project/project/src/App.js
+++ Project/project/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Birthmain } from './Pages/birthday/Birthmain';
+import { Emailmain } from './Pages/email/Emailmain';
 import {Home} from "./Pages/Home";
+import { Post } from './Pages/post_page/Post';
+import { Signup_page } from './Pages/signup/Signup_page';
+
+
 
 function App() {
   return (
     <div className="App">
-     <Home></Home>
+     <Post></Post>
     </div>
   );
 }
